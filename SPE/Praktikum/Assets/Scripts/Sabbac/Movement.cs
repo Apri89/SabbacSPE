222a5a39f003453aa01d7c819a9b4e3aa8075015
khalasar <andre_stuhrmann@web.de>
2013-12-22 20:28:36 +0100

    Modify Scene:level2, and change checking ball groundend. Check with Raycasting

--- SPE/Praktikum/Assets/Scripts/Sabbac/Movement.cs
+++ SPE/Praktikum/Assets/Scripts/Sabbac/Movement.cs
@@ -5,19 +5,28 @@ public class Movement : MonoBehaviour {
 		private bool grounded = false;
 	    public float movementSpeed = 4.0f;
 		public float power = 70.0f;
+		public float height;
 	
 	
 	//FixedUpdate should be used instead of Update when dealing with Rigidbody..
-	void FixedUpdate () {
-		//if (grounded){
+	void Update () {
+		Debug.DrawRay(transform.position, -Vector3.up*height, Color.green);
+		RaycastHit hit;
+		
+		if (Physics.Raycast(transform.position, -Vector3.up,out hit ,height) || Physics.Raycast(transform.position, -Vector3.left,out hit ,height)){
 	        Vector3 movement = (Input.GetAxis("Horizontal") * Vector3.right * movementSpeed)
 							 + (Input.GetAxis("Vertical") * Vector3.forward * movementSpeed);			
 			//Vector3 Force, ForceMode
 	        rigidbody.AddForce(movement, ForceMode.Impulse);
-			if (Input.GetButtonDown ("Jump") && grounded){
-				rigidbody.AddForce(Vector3.up * power,ForceMode.Impulse); 
-	    	}
-		//}
+			
+			Debug.Log("Grounded");
+		
+			if(hit.transform.gameObject.CompareTag("Ground")){
+				if (Input.GetButtonDown ("Jump")){
+					rigidbody.AddForce(Vector3.up * power,ForceMode.Impulse); 
+				}	
+			}
+		}
 	}
 	void OnCollisionStay(Collision collisionInfo) {
 		grounded = true;
