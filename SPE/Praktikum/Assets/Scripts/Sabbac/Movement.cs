b10c12128b8f4d50e66e3465b4a1c2d790b8d720
khalasar <andre_stuhrmann@web.de>
2013-12-23 12:11:29 +0100

    add a hollow cylinder to the scene

--- SPE/Praktikum/Assets/Scripts/Sabbac/Movement.cs
+++ SPE/Praktikum/Assets/Scripts/Sabbac/Movement.cs
@@ -4,34 +4,34 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 		private bool grounded = false;
 	    public float movementSpeed = 4.0f;
-		public float power = 70.0f;
+		public float power = 50.0f;
 		public float height;
 	
 	
 	//FixedUpdate should be used instead of Update when dealing with Rigidbody..
-	void Update () {
-		Debug.DrawRay(transform.position, -Vector3.up*height, Color.green);
-		RaycastHit hit;
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
 		
-		if (grounded){
-	        Vector3 movement = (Input.GetAxis("Horizontal") * Vector3.right * movementSpeed)
+		//Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		
+	       Vector3 movement = (Input.GetAxis("Horizontal") * Vector3.right * movementSpeed)
 							 + (Input.GetAxis("Vertical") * Vector3.forward * movementSpeed);			
 			//Vector3 Force, ForceMode
-	        rigidbody.AddForce(movement, ForceMode.Impulse);
-			
-			//Debug.Log("Grounded");
-		
-			if (Input.GetButtonDown ("Jump")){
+	        rigidbody.AddForce(movement * movementSpeed * Time.deltaTime);
+					
+			if (Input.GetButtonDown ("Jump") && grounded){
 				rigidbody.AddForce(Vector3.up * power,ForceMode.Impulse); 
 			}	
-		}
 	}
+	
 	void OnCollisionStay(Collision collisionInfo) {
 		Debug.Log("Collision: "+collisionInfo.collider.tag);
 		if(collisionInfo.collider.tag == "Ground"){ 
 			grounded = true;
 		}
 	}
+	
 	void OnCollisionExit(Collision collisionInfo) {
 		if(collisionInfo.collider.tag == "Ground"){ 
 			grounded = false;
